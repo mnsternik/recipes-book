e32da7812a99a0ab0a1df6b442580dcf541e691f
Marcin Sternik <mnsternik@gmail.com>
2023-10-25 23:29:38 +0200

    Saving and fetching data added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,58 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeSelectInfoComponent } from "./recipes/recipe-select-info/recipe-select-info.component";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
+import { RecipesResolverService } from "./recipes/recipes-resolver.service";
+import { AuthComponent } from "./auth/auth.component";
 
 const appRoutes: Routes = [
-    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
     {
-        path: 'shopping-list', component: ShoppingListComponent, children: [
-            { path: 'edit', component: ShoppingEditComponent }
+        path: '',
+        redirectTo: '/recipes',
+        pathMatch: 'full'
+    },
+    {
+        path: 'auth',
+        component: AuthComponent
+    },
+    {
+        path: 'shopping-list',
+        component: ShoppingListComponent,
+        children: [
+            {
+                path: 'edit',
+                component: ShoppingEditComponent
+            }
         ]
     },
     {
-        path: 'recipes', component: RecipesComponent, children: [
-            { path: '', component: RecipeSelectInfoComponent, pathMatch: 'full' },
-            { path: 'new', component: RecipeEditComponent },
-            { path: ':id', component: RecipeDetailComponent },
-            { path: ':id/edit', component: RecipeEditComponent}
+        path: 'recipes',
+        component: RecipesComponent,
+        children: [
+            {
+                path: '',
+                component: RecipeSelectInfoComponent,
+                pathMatch: 'full'
+            },
+            {
+                path: 'new',
+                component: RecipeEditComponent
+            },
+            {
+                path: ':id',
+                component: RecipeDetailComponent,
+                resolve: [RecipesResolverService]
+            },
+            {
+                path: ':id/edit',
+                component: RecipeEditComponent,
+                resolve: [RecipesResolverService]
+            }
         ]
     }
 ]
