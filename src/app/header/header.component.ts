fc5ee3fe9313805294d30f5f9005d40e85534d78
Marcin Sternik <mnsternik@gmail.com>
2023-11-03 14:48:50 +0100

    Authentication section added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,24 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-   collapsed = true;
+export class HeaderComponent implements OnInit, OnDestroy {
+    private authSubscription: Subscription;
+    isAuthenticated = false; 
+    collapsed = true;
 
-   constructor(private dataStorageService: DataStorageService) { 
 
-   }
+    constructor(private dataStorageService: DataStorageService, private authService: AuthService) { }
 
-   onFetchRecipes() {
-    this.dataStorageService.fetchRecipes().subscribe();
-   }
+    ngOnInit(): void {
+        this.authSubscription = this.authService.user.subscribe(user => {
+                this.isAuthenticated = !user ? false : true; 
+            }
+        )
+    }
 
-   onSaveRecipes() {
-    this.dataStorageService.storeRecipes(); 
-   }
+    onFetchRecipes() {
+        this.dataStorageService.fetchRecipes().subscribe();
+    }
+
+    onSaveRecipes() {
+        this.dataStorageService.storeRecipes();
+    }
+
+    onLogout() {
+        this.authService.logout(); 
+    }
+
+    ngOnDestroy(): void {
+        this.authSubscription.unsubscribe(); 
+    }
 
 }
