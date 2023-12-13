3b9b715a2cdfeeea1a279101b99d3d1026f0f61c
Marcin Sternik <mnsternik@gmail.com>
2023-12-13 08:39:16 +0100

    New properties added

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -5,11 +5,37 @@ export class Recipe {
     public description: string;
     public imagePath: string;
     public ingredients: Ingredient[];
+    public authorId?: string;
+    public id?: string;
+    public instructions: string;
+    public price?: number;
+    public preparationTime?: number;
+    public tags?: string[];
+    public calories?: number;
 
-    constructor(name: string, desc: string, imagePath: string, ingredients: Ingredient[]) {
+    constructor(
+        name: string,
+        desc: string,
+        imagePath: string,
+        ingredients: Ingredient[],
+        authorId: string,
+        id: string,
+        instructions: string,
+        price: number,
+        preparationTime: number,
+        tags: string[],
+        calories: number
+    ) {
         this.name = name;
         this.description = desc;
         this.imagePath = imagePath;
         this.ingredients = ingredients;
+        this.authorId = authorId;
+        this.id = id;
+        this.instructions = instructions;
+        this.price = price; 
+        this.preparationTime = preparationTime;
+        this.tags = tags;
+        this.calories = calories;
     }
 }
