1543d16dd73cd057c717c5ae3090c58961de2ede
Marcin Sternik <mnsternik@gmail.com>
2023-12-13 08:29:41 +0100

    Favroties and author filters added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-
-import { RecipceService } from '../recipe.service';
-
-import { Recipe } from '../recipe.model';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Subscription } from 'rxjs';
+
+import { LocalRecipceService } from '../local-recipes.service';
+import { Recipe } from '../recipe.model';
+import { AuthService } from 'src/app/auth/auth.service';
+import { LocalFavoritesService } from '../favorites/local-favorites.service';
+import { DataStorageRecipesService } from 'src/app/data-storage/ds-recipes.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -13,28 +15,54 @@ import { Subscription } from 'rxjs';
 })
 export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[] = [];
-  subscription: Subscription;
+  filteredRecipes: Recipe[]; 
+  favorites: string[]; 
+  recipeSubscription: Subscription;
+  searchedRecipeName = ''; 
+  userId: string; 
+  mode: 'all' | 'favorites' | 'my'; 
 
   constructor(
-    private recipceService: RecipceService,
-    private route: ActivatedRoute,
-    private router: Router) { }
+    private authService: AuthService,
+    private localRecipceService: LocalRecipceService,
+    private localFavoritesService: LocalFavoritesService,
+    private dsRecipesService: DataStorageRecipesService,
+    private route: ActivatedRoute ) { }
 
   ngOnInit(): void {
-    this.recipes = this.recipceService.getRecipes();
-    this.subscription = this.recipceService.recipesChanged
-      .subscribe(
+    this.recipes = this.localRecipceService.getRecipes();
+    this.favorites = this.localFavoritesService.getFavorites(); 
+    this.userId = this.authService.user.value ? this.authService.user.value.id : ''; 
+    this.recipeSubscription = this.localRecipceService.recipesChanged.subscribe(
         (recipes: Recipe[]) => {
           this.recipes = recipes;
+          this.filterRecipes();
         }
-      )
+      ) 
+    this.route.queryParams.subscribe(
+      (params: Params) => {
+        this.mode = params.mode; 
+        this.filterRecipes();
+    })
+  }
+
+  onRefresh() {
+    this.dsRecipesService.fetchRecipes().subscribe(); 
+  }
+
+  private filterRecipes(): void {
+    if (this.mode === 'my') {
+      this.filteredRecipes = this.recipes.filter(recipe => recipe.authorId === this.userId); 
+    }
+    else if (this.mode === 'favorites') {
+      this.filteredRecipes = this.recipes.filter(recipe => this.localFavoritesService.checkIfRecipeIsFavorite(recipe.id)); 
+    }
+    else {
+      this.filteredRecipes = [...this.recipes]; 
+    }
   }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
-
-  onNewRecipe() {
-    this.router.navigate(['new'], { relativeTo: this.route });
+    this.recipeSubscription.unsubscribe();
   }
 }
