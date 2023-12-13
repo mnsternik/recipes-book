fba20dc102471cdaf23833714276f6a5113083db
Marcin Sternik <mnsternik@gmail.com>
2023-12-13 08:40:16 +0100

    New recipes services applied

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -3,17 +3,17 @@ import { Resolve } from "@angular/router";
 import { ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
 
 import { Recipe } from "./recipe.model";
-import { DataStorageService } from "../shared/data-storage.service";
-import { RecipceService } from "./recipe.service";
+import { DataStorageRecipesService } from "../data-storage/ds-recipes.service";
+import { LocalRecipceService } from "./local-recipes.service";
 
 @Injectable({ providedIn: 'root' })
 export class RecipesResolverService implements Resolve<Recipe[]> {
-    constructor(private dataStorageService: DataStorageService, private recipesService: RecipceService) { }
+    constructor(private dsRecipesService: DataStorageRecipesService, private localRecipesService: LocalRecipceService) { }
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        const recipes = this.recipesService.getRecipes(); 
+        const recipes = this.localRecipesService.getRecipes(); 
         if (recipes.length === 0) {
-            return this.dataStorageService.fetchRecipes();
+            return this.dsRecipesService.fetchRecipes();
         } else {
             return recipes;
         }
