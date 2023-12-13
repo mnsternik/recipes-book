bd765075297b19c235b1d8ebb6b15f0909700f41
Marcin Sternik <mnsternik@gmail.com>
2023-12-13 08:37:39 +0100

    Updating recipe fixed

---
+++ src/app/recipes/local-recipes.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+
+import { Recipe } from "./recipe.model";
+
+@Injectable({ providedIn: 'root' })
+export class LocalRecipceService {
+
+    private recipes: Recipe[] = [];
+    
+    recipesChanged = new Subject<Recipe[]>; 
+
+    getRecipes() {
+        return structuredClone(this.recipes);
+    }
+
+    getRecipe(id: string) {
+        const recipeIndex = this.recipes.findIndex(recipe => recipe.id === id); 
+        return structuredClone(this.recipes)[recipeIndex];
+    }
+
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes; 
+        this.recipesChanged.next(structuredClone(this.recipes));
+    }
+
+    addRecipe(recipe: Recipe) {
+        this.recipes.push(recipe); 
+        this.recipesChanged.next(structuredClone(this.recipes));
+    }
+
+    updateRecipe(id: string, recipe: Recipe) {
+        const updatedRecipeIndex = this.recipes.findIndex(recipe => recipe.id === id); 
+        this.recipes[updatedRecipeIndex] = recipe; 
+        this.recipesChanged.next(structuredClone(this.recipes));
+    }
+
+    removeRecipe(id: string) {
+        const removedRecipeIndex = this.recipes.findIndex(recipe => recipe.id === id); 
+        this.recipes.splice(removedRecipeIndex, 1); 
+        this.recipesChanged.next(structuredClone(this.recipes));
+    }
+}
