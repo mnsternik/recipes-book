18a9bf0e1b614b885f353b9f6190a346c87a28c2
Marcin Sternik <mnsternik@gmail.com>
2024-03-12 23:14:44 +0100

    Card style added

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -6,8 +6,20 @@ import { Recipe } from '../../recipe.model';
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.css']
 })
-export class RecipeItemComponent {
+export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe; 
   @Input() index: number; 
 
+  maxTagsAmount = 3; 
+  visiableTags: string[] = []; 
+  tagsAmountExceeded: boolean; 
+
+  ngOnInit(): void {
+      this.tagsAmountExceeded = this.recipe.tags.length > this.maxTagsAmount; 
+      if (this.tagsAmountExceeded) {
+          this.visiableTags = this.recipe.tags.slice(0, this.maxTagsAmount); 
+      } else {
+          this.visiableTags = this.recipe.tags.slice(); 
+      }
+  }
 }
